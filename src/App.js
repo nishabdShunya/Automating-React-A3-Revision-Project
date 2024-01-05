f406d137ff49680623e9ef2f9a02353786c9e79c
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-01-05 23:43:25 +0530

    Add. A3-T3-Add User Form-With Solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // Write your code at relevant places in the code below:
 
 import React from "react";
+import AddUser from "./components/Users/AddUser";
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
