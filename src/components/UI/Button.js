1e5bb044e0daecc846c6deb81cb769b35533650f
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-01-06 02:18:22 +0530

    Add. A3-T5-Reusable Button-With Solution

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,1 +1,12 @@
-// Write your code here:
+import React from "react";
+import "./Button.css";
+
+const Button = (props) => {
+  return (
+    <button className="button" type={props.type}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
