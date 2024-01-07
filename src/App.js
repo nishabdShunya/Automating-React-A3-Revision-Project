564278d25dbdc52afd0a72393854967da9a3b497
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-01-07 22:58:09 +0530

    Add. A3-T8-Rendering Users List-With Solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// Write your code at relevant places in the code below:
-
 import React from "react";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,1 +1,21 @@
-// Write your code here:
+import React from "react";
+import Card from "../UI/Card";
+import "./UsersList.css";
+
+const UsersList = (props) => {
+  return (
+    <Card className="users">
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <li>
+              ${user.name} (${user.age} years old)
+            </li>
+          );
+        })}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
